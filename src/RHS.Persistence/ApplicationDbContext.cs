e07b5b429bad8ce3c41721d468413f2bc5dff65a
RasmusHS <rasshoey@gmail.com>
2025-11-27 13:23:09 +0100

    Add seed data for Resume and Project entities in ApplicationDbContext

--- src/RHS.Persistence/ApplicationDbContext.cs
+++ src/RHS.Persistence/ApplicationDbContext.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RHS.Application.Data;
+using RHS.Domain.Common.ValueObjects;
 using RHS.Domain.Resume;
 using RHS.Domain.Resume.Entities;
+using RHS.Domain.Resume.ValueObjects;
+using System.Drawing;
 
 namespace RHS.Persistence;
 
@@ -22,5 +25,61 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        var resumeId = ResumeId.Create().Value;
+        modelBuilder.Entity<ResumeEntity>()
+            .HasData(
+                resumeId, 
+                "TestIntro", 
+                FullName.Create("John", "Doe").Value, 
+                Address.Create("TestStreet 1", "9000", "Aalborg").Value, 
+                Email.Create("TestMail@TestDomain.dk").Value, 
+                "https://github.com/RasmusHS", 
+                "https://www.linkedin.com/in/rasmus-h%C3%B8y-s-40079513a/", 
+                ImageToByteArray(Path.Combine(AppContext.BaseDirectory, "SeedFiles", "IMG_0633.jpg")),
+                DateTime.UtcNow,
+                DateTime.UtcNow);
+        
+        modelBuilder.Entity<ResumeEntity>()
+            .HasData(ResumeEntity.Create(
+                "TestIntro", 
+                FullName.Create("John", "Doe").Value, 
+                Address.Create("TestStreet 1", "9000", "Aalborg").Value, 
+                Email.Create("TestMail@TestDomain.dk").Value, 
+                "https://github.com/RasmusHS", 
+                "https://www.linkedin.com/in/rasmus-h%C3%B8y-s-40079513a/", 
+                ImageToByteArray(Path.Combine(AppContext.BaseDirectory, "SeedFiles", "IMG_0633.jpg"))));
+        
+        var projectId1 = ProjectId.Create().Value;
+        var projectId2 = ProjectId.Create().Value;
+        
+        modelBuilder.Entity<ProjectEntity>()
+            .HasData(
+                projectId1,
+                resumeId,
+                "TestProjectTitle1",
+                "TestProjectDescription1",
+                "TestProjectUrl1.dk",
+                ImageToByteArray(Path.Combine(AppContext.BaseDirectory, "SeedFiles", "10f.gif")),
+                true,
+                DateTime.UtcNow,
+                DateTime.UtcNow);
+        
+        modelBuilder.Entity<ProjectEntity>()
+            .HasData(
+                projectId2,
+                resumeId,
+                "TestProjectTitle2",
+                "TestProjectDescription2",
+                "TestProjectUrl2.dk",
+                ImageToByteArray(Path.Combine(AppContext.BaseDirectory, "SeedFiles", "10f.gif")),
+                true,
+                DateTime.UtcNow,
+                DateTime.UtcNow);
+    }
+
+    private byte[] ImageToByteArray(string imagePath)
+    {
+        return File.ReadAllBytes(imagePath);
     }
 }
